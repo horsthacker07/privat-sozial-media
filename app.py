69899f876a41b9e002ed00e392bfe290370d074a
horsthacker07 <horsthacker07@gmail.com>
2025-04-12 17:04:55 +0200

    added login, logout, session

--- app.py
+++ app.py
@@ -1,7 +1,32 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for, session
 
 app = Flask(__name__)
+app.secret_key = "the_secret_key"  # Muss für Sessions gesetzt sein
+
+# Fesetgelegtes Passwort
+PASSWWORD = "secret_password"
 
 @app.route('/')
+def root():
+    if session.get('logged_in'):
+        return redirect(url_for('home'))
+    return render_template('login.html')
+
+@app.route('/login', methods=['POST'])
+def login():
+    if request.form['password'] == PASSWORD:
+        session['logged_in'] = True
+        return redirect(url_for('home'))
+    else:
+        return render_template('login.html', error="Falsches Passwort.")
+
+@app.route('/home')
 def home():
+    if not session.get('logged_in'):
+        return redirect(url_for('root'))
     return render_template('home.html')
+
+@app.route('/logout')
+def logout():
+    session.clear()
+    return redirect(url_for('root'))
