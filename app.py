7b3a426e527b2129725621f746378aefc6d6e4f9
horsthacker07 <horsthacker07@gmail.com>
2025-04-13 00:31:42 +0200

    Implement global chat functionality with message posting and loading

--- app.py
+++ app.py
@@ -1,10 +1,29 @@
-from flask import Flask, render_template, request, redirect, url_for, session
+from flask import Flask, render_template, jsonify, request, redirect, url_for, session
 
 app = Flask(__name__)
 app.secret_key = "the_secret_key"  # Muss für Sessions gesetzt sein
 
 # Fesetgelegtes Passwort
 PASSWORD = "secret_password"
+
+# JSON-Datenbank für global-chat
+global_chat = []
+
+
+@app.route('/global_chat_db', methods=['GET'])
+def get_messages():
+    return jsonify(global_chat_db=global_chat)
+
+
+@app.route('/global_chat_db', methods=['POST'])
+def add_message():
+    message = request.form['message']
+    if message:
+        global_chat.append(message)
+    #return redirect(url_for('global_chat_db'))
+
+
+### ab hier sind die Routen für home, login und logout ###
 
 @app.route('/')
 def root():
