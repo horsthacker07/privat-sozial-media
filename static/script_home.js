149c1152896913666cc374bb1a8d39826e22cf03
horsthacker07 <horsthacker07@gmail.com>
2025-04-14 13:48:31 +0200

    Restore socket connection script in home.html and ensure alert functionality in script_home.js

--- static/script_home.js
+++ static/script_home.js
@@ -1,1 +1,25 @@
 alert('Grrrrr');
+
+$(document).ready(function() {
+    var socket = io({ transports: ['websocket'], upgrade: false });
+
+    socket.on('connect', function() {
+        console.log("Socket connected!");
+        socket.send("User connected!");
+    });
+
+    socket.on('message', function(data) {
+        console.log("Nachricht empfangen:", data);
+        $('#messages_new').append($('<p>').text(data));
+    });
+
+    $('#chatForm').on('submit', function(e) {
+        e.preventDefault(); // verhindert Seitenreload
+        const message = $('#message').val();
+        if (message) {
+            socket.send(message);
+            $('#message').val('');
+            console.log("gesendet!!!");
+        }
+    });
+});
